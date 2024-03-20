64dd6f4ace83e25816575f2f6bc673c5cf327561
Shubham <mynameshubham23@gmail.com>
2024-03-21 00:49:47 +0530

    Adds data preprocessing

--- data.py
+++
@@ -1,9 +0,0 @@
-import datasets
-
-# Loads the dataset from hugging face
-#ds = datasets.load_dataset("rainerberger/Mri_segmentation")
-#ds.save_to_disk("../datasets/huggingface/mri_segmentation_dataset")
-
-mydata = datasets.load_from_disk("../datasets/huggingface/mri_segmentation_dataset")
-
-print(mydata)
