ed8b25f361c6510b8b5102e033451f9b39839666
Shubham <mynameshubham23@gmail.com>
2024-03-25 10:14:00 +0530

    Implemented Unet architecture according to its research paper

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from tensorflow.keras.layers import Input, Concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, Flatten
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras import backend as K
@@ -12,8 +12,68 @@ K.set_image_data_format('channels_last')
 
 def unet_model():
     inputs = Input((512, 512, 1))
+
+    # Contracting layers
     conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
-    
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv1)
+    maxpool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(maxpool1)
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv2)
+    maxpool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(256, (3, 3), activation='relu', padding='same')(maxpool2)
+    conv3 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3)
+    maxpool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(512, (3, 3), activation='relu', padding='same')(maxpool3)
+    conv4 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv4)
+    maxpool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(maxpool4)
+    conv5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(conv5)
+
+    # Expansive Layers
+    conv_transpose1 = Conv2DTranspose(512, (2, 2), padding='same')(conv5)
+    # dont quite understand this, concatenate should make the channel dimension 1024 making it impossible to convolve with 512 channel dims
+    conv_cat1 = Concatenate(axis=3)([conv_transpose1, conv4])
+    conv6 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv_cat1)
+    conv6 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv6)
+
+    conv_transpose2 = Conv2DTranspose(256, (2, 2), padding='same')(conv6)
+    conv_cat2 = Concatenate(axis=3)([conv_transpose2, conv3])
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv_cat2)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv7)
+
+    conv_transpose3 = Conv2DTranspose(128, (2, 2), padding='same')(conv7)
+    conv_cat3 = Concatenate(axis=3)([conv_transpose3, conv2])
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv_cat3)
+    conv8 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv8)
+
+    conv_transpose4 = Conv2DTranspose(64, (2, 2), padding='same')(conv8)
+    conv_cat4 = Concatenate(axis=3)([conv_transpose4, conv1])
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv_cat4)
+    conv9 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv9)
+
+    # last layer
+    conv10 = Conv2D(2, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs, conv10)
+    model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+
+    return model
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = Flatten(y_true)
+    y_pred_f = Flatten(y_pred)
+    intersection = tf.keras.sum(y_true_f * y_pred_f)
+    return (2. * intersection + 1) / (tf.keras.sum(y_true_f) + tf.keras.sum(y_pred_f) + 1)
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
 
 
 def train():
